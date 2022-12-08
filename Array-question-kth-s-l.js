9da75ecd98db7f0463740e66e7cb8142946aad3c
shikhar68 <shikharparikh97@gmail.com>
2022-12-08 22:16:12 +0530

    array

--- Array-question-kth-s-l.js
+++ Array-question-kth-s-l.js
@@ -8,8 +8,14 @@
 //   largest: 5,
 // }
 
-//ALGO:
+//ALGO: neww update
 // 1. Create a function with two parameter arr and k
 // 2. Sort the arr
 // 3. Now return the arr[k] as smalest value
 // 4. And return arr[array.length-k] as the largest value
+
+function result(arr, k) {
+  arr.sort();
+  console.log(arr);
+}
+result([2, 1, 4, 3, 6, 5, 7], 3);
