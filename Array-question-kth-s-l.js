23ed528844af73a1fc57c633717b9ab2a34b2122
shikhar68 <shikharparikh97@gmail.com>
2022-12-08 21:42:02 +0530

    array question

---
+++ Array-question-kth-s-l.js
@@ -0,0 +1,15 @@
+// To find kth smallest or largest element in an array
+// const arr = [2,1,4,3,6,5,7];
+// const input = findKthSmallestOrLargest([2,1,4,3,6,5,7], 3);
+
+// // Output
+// {
+//   smallest: 3,
+//   largest: 5,
+// }
+
+//ALGO:
+// 1. Create a function with two parameter arr and k
+// 2. Sort the arr
+// 3. Now return the arr[k] as smalest value
+// 4. And return arr[array.length-k] as the largest value
