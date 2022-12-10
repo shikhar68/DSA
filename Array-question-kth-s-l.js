b7a1c0786bc42e9627609a52803e2a081fa70ae5
shikhar68 <shikharparikh97@gmail.com>
2022-12-11 01:54:16 +0530

    0 1 2 question

--- Array-question-kth-s-l.js
+++ Array-question-kth-s-l.js
@@ -8,14 +8,27 @@
 //   largest: 5,
 // }
 
-//ALGO: neww update
+//ALGO: new update
 // 1. Create a function with two parameter arr and k
 // 2. Sort the arr
-// 3. Now return the arr[k] as smalest value
+// 2.1. Creare 2 for loop
+// 2.2. compare arr[i] > arr[j] is yes than swap them
+// 3. Now return the arr[k-1] as smalest value
 // 4. And return arr[array.length-k] as the largest value
 
 function result(arr, k) {
-  arr.sort();
-  console.log(arr);
+  for (i = 0; i < arr.length; i++) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  console.log("sorted array:", arr);
+
+  console.log("kth smallest:", arr[k - 1]);
+  console.log("kth largest:", arr[arr.length - k]);
 }
 result([2, 1, 4, 3, 6, 5, 7], 3);
--- Sort-0-1-2.js
+++ Sort-0-1-2.js
@@ -1,19 +1,59 @@
 //Sort an array of 0s, 1s and 2s
 
-input [1,2,1,1,1,0,0,0,1,2]
-output [0,0,0,1,1,1,1,2,2,2]
+input[(1, 2, 1, 1, 1, 0, 0, 0, 1, 2)];
+output[(0, 0, 0, 1, 1, 1, 1, 2, 2, 2)];
 
-input [1,0,1]
-output [0,1,1]
+input[(1, 0, 1)];
+output[(0, 1, 1)];
 
-input [2,0,1,2]
-output [0,1,2,2]
+input[(2, 0, 1, 2)];
+output[(0, 1, 2, 2)];
 
 // Algo:
-// 1. Create a new blank array2.
-// 2. Apply 1st loop on array.
-// 3. Create condition for 0 if array[i]===0 push it into new array2.
-// 4. Apply 2nd loop on array.
-// 5. Create condition for 1 if array[i]===1 push it into new array2.
-// 6. Apply 3rd loop on array.
-// 7. Create condition for 2 if array[i]===2 push it into new array2.
+// 1. Create 3 blank arrays.
+// 2. Apply 1st loop on main array.
+// 3. Create condition for 0 if array[i]<1 push it into new arr1.
+// 4. Create condition for 1 if array[i]=1 push it into new arr2.
+// 5. Create condition for 2 if array[i]>1 push it into new arr3.
+// 6. Mearge all 3 array
+
+//Using one loop
+function result(arr) {
+  let arr1 = [];
+  let arr2 = [];
+  let arr3 = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < 1) {
+      arr1.push(arr[i]);
+    } else if (arr[i] > 1) {
+      arr3.push(arr[i]);
+    } else if ((arr[i] = 1)) {
+      arr2.push(arr[i]);
+    }
+  }
+  myArray = [...arr1, ...arr2, ...arr3];
+  console.log(myArray);
+}
+result([1, 2, 1, 0, 2, 2, 0, 1, 1, 2]);
+//Algo 2: 2nd way using swaping technique
+
+// 2.1. Create 2 for loop
+// 2.2. compare arr[i] > arr[j] is yes than swap them
+// store = arr[i];
+// arr[i] = arr[j];
+// arr[j] = store;
+// 2.3 if not continoue the loop
+
+function result(arr) {
+  for (i = 0; i < arr.length; i++) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  console.log(arr);
+}
+result([1, 2, 1, 1, 1, 0, 0, 0, 1, 2]);
